c6a4c53007b0db95d90eb159fdb741bf4b1d77c7
AntonAcorn <ShustikovAnimation@gmail.com>
2022-12-19 11:22:16 +0300

    Add new migration

--- src/main/resources/db/migration/V00001__add_tg_user_table.sql
+++ src/main/resources/db/migration/V00001__add_tg_user_table.sql
@@ -4,5 +4,6 @@ DROP TABLE IF EXISTS tg_user;
 -- Create tg_user table
 CREATE TABLE tg_user (
                          chat_id VARCHAR(100),
-                         active BOOLEAN
+                         active BOOLEAN,
+                         PRIMARY KEY (chat_id)
 );
