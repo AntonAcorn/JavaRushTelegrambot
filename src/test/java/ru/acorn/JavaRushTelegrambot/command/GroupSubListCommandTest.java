478cdecc12f3b48c42cc6be067f8480bd63b61c0
AntonAcorn <ShustikovAnimation@gmail.com>
2022-12-20 12:33:15 +0300

    add GroupSubListCommandTest test

---
+++ src/test/java/ru/acorn/JavaRushTelegrambot/command/GroupSubListCommandTest.java
@@ -0,0 +1,73 @@
+package ru.acorn.JavaRushTelegrambot.command;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.acorn.JavaRushTelegrambot.repository.entity.GroupSub;
+import ru.acorn.JavaRushTelegrambot.repository.entity.TelegramUser;
+import ru.acorn.JavaRushTelegrambot.service.SendBotMessageService;
+import ru.acorn.JavaRushTelegrambot.service.SendBotMessageServiceImpl;
+import ru.acorn.JavaRushTelegrambot.service.TelegramUserService;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static ru.acorn.JavaRushTelegrambot.command.CommandName.LIST_GROUP_SUB;
+
+class GroupSubListCommandTest {
+
+    @Test
+    void shouldProperlyShowGroupList() {
+        //given
+        TelegramUser user = new TelegramUser();
+        user.setActive(true);
+        user.setChatId("1");
+
+        List<GroupSub> groupSubList = new ArrayList<>();
+        groupSubList.add(populateGroupSub(1, "gs1"));
+        groupSubList.add(populateGroupSub(2, "gs2"));
+        groupSubList.add(populateGroupSub(3, "gs3"));
+        groupSubList.add(populateGroupSub(4, "gs4"));
+
+        user.setGroupSubs(groupSubList);
+
+        SendBotMessageService sendBotMessageService = Mockito.mock(SendBotMessageServiceImpl.class);
+        TelegramUserService telegramUserService = Mockito.mock(TelegramUserService.class);
+
+        Mockito.when(telegramUserService.findByChatId(user.getChatId())).thenReturn(Optional.of(user));
+
+        GroupSubListCommand command = new GroupSubListCommand(telegramUserService, sendBotMessageService);
+
+        Update update = new Update();
+        Message message = Mockito.mock(Message.class);
+        Mockito.when(message.getChatId()).thenReturn(Long.valueOf(user.getChatId()));
+        Mockito.when(message.getText()).thenReturn(LIST_GROUP_SUB.getCommandName());
+        update.setMessage(message);
+
+        String collectedGroups = "All subscriptions to the group: \n\n" +
+                user.getGroupSubs().stream()
+                        .map(it -> "Group: " + it.getTitle() + " Group ID: " + it.getId() + " \n")
+                        .collect(Collectors.joining());
+
+        //when
+        command.execute(update);
+
+        //then
+        Mockito.verify(sendBotMessageService).sendMessage(user.getChatId(), collectedGroups);
+    }
+
+    private GroupSub populateGroupSub(int id, String title) {
+        GroupSub group = new GroupSub();
+        group.setId(id);
+        group.setTitle(title);
+        return group;
+    }
+
+
+}
